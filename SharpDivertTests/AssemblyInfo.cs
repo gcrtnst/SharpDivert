56a8fb5d50c137b73856caaa54994b94ad25d9b8
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-12-08 13:05:47 +0900

    Update test dependencies

---
+++ SharpDivertTests/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * AssemblyInfo.cs
+ * Copyright gcrtnst
+ *
+ * This file is part of SharpDivert.
+ *
+ * SharpDivert is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * SharpDivert is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SharpDivert.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SharpDivert is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * SharpDivert is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with SharpDivert; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: TestDataSourceDiscovery(TestDataSourceDiscoveryOption.DuringExecution)]
